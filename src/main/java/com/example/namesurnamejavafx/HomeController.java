b56098b891b32f1bf201f268a1c55d62c9dd7187
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-03-23 14:07:26 +0100

    Login

--- src/main/java/com/example/namesurnamejavafx/HomeController.java
+++ src/main/java/com/example/namesurnamejavafx/HomeController.java
@@ -1,7 +1,14 @@
 package com.example.namesurnamejavafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeController {
     @FXML
@@ -11,4 +18,12 @@ public class HomeController {
         welcomeLabel.setText("Hello " +userName);
     }
 
+    public void logoutButtonOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loginView.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) welcomeLabel.getScene().getWindow();
+        stage.setScene(new Scene(root, 400, 300));
+
+    }
 }
