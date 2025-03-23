b61969251433572f1ebb13e115635573d5c23fef
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-03-23 16:28:46 +0100

    you can write the userinformation in the text. if you want to try this userName : ebubekir password : 1234

--- src/main/java/com/example/namesurnamejavafx/ProfileController.java
+++ src/main/java/com/example/namesurnamejavafx/ProfileController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ProfileController {
@@ -18,13 +20,23 @@ public class ProfileController {
     private TextField name;
     @FXML
     private TextField surname;
+    @FXML
+    private Label information;
 
     public void setUserName(String userName){
         profileName.setText(userName);
     }
-    public void saveOnAction(ActionEvent actionEvent) {
+    public void saveOnAction(ActionEvent actionEvent) throws IOException {
         System.out.println("Name : " + name.getText()+ " Surname : " + surname.getText());
+        String nameText = name.getText();
+        String surnameText = surname.getText();
+        File file = new File("userInformation.txt");
+        FileWriter writer = new FileWriter(file, true);
+        writer.write("\nName: "+nameText+"\nSurname: "+surnameText);
+        writer.close();
 
+
+        information.setText("Information successfully saved");
     }
 
     public void logoutButtonOnAction(ActionEvent actionEvent) throws IOException {
